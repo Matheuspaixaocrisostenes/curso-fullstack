f2cf13baf6d7005997fd2cc6804c3c4bc8e14639
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2023-07-17 05:53:20 -0300

    aprendendo sobre condicoes

---
+++ webmaster-frontend/javascript/aula06/functions.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+    // var nome = 'Guilherme'
+
+    // if(nome == 'joão'){
+    //     alert('condição verdadeira')
+    // }else if(nome == 'Guilherme'){
+    //     alert('condição verdadeira')
+    // }else{
+    //     alert('condição falsa')
+    // }
+
+    // var idade = 2016 - 1993
+
+    // if(idade > 23){
+    //     alert('Eu tenho mais de 23 anos de idade')
+    // }else{
+    //     alert('Eu sou menor ou igual a 23 anos de idade')
+    // }
+
+    // var  number = '23'
+
+    // if(number === 23){
+    //     console.log('Verdade');
+    // }else{
+    //     console.log('Falso');
+    // }
+
+    var nome = "Guilherme"
+
+    if(nome != 'Guilherme'){
+        console.log('Verdade');
+    }
+}

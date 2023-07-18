015c443c910f0ba69a8169947e41ef05d05388e2
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2023-07-18 05:43:24 -0300

    aprendendo lacos de repeticao

---
+++ webmaster-frontend/javascript/aula07/functions.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    // var repeticao = 0
+
+    // while(repeticao < 10){
+    //     console.log("Olá mundo numero: " + repeticao);
+    //     repeticao++
+    // }
+
+    // console.log("Cheguei até o final");
+
+    // for(var numero = 0; numero < 10; numero++){
+    //     console.log("Olá mundo");
+    // }
+
+    // var nome = ''
+
+    // do{
+    //     console.log("Olá mundo!");
+    // }while(nome == "Guilherme"){
+
+    // }
+}

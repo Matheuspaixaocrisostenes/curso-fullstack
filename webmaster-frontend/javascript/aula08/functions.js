afc795d8ec10b260ef8b2c1353a212b7ab176a3d
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2023-07-18 06:04:38 -0300

    aprendendo sobre functions

--- webmaster-frontend/javascript/aula08/functions.js
+++ webmaster-frontend/javascript/aula08/functions.js
@@ -1,3 +1,26 @@
 window.onload = function(){
-    
+
+    // function realizarConta(num1,num2){
+    //     return num1 + num2
+    // }
+
+    // var resultado = realizarConta(10,10)
+    // console.log(resultado);
+
+    // var variavel = function(){
+    //     console.log('Minha função através da variável');
+    // }
+
+    // variavel()
+
+    // var obj = {'nome':'Guilherme', 'idade': 23, 'func': function(){
+    //     console.log('Guilherme');
+    // }}
+    // obj.func()
+
+    var variavel = []
+
+    variavel.obj = {}
+    variavel.obj.name = 'Guilherme'
+    console.log(variavel.obj.name);
 }

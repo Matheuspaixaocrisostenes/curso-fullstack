4e0996796f11385d85115854ccc064dbf825794f
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2023-07-24 05:49:07 -0300

    aprendendo como manipular o css via jquery

---
+++ webmaster-frontend/jquery/aula02/functions.js
@@ -0,0 +1,8 @@
+$(function(){
+    // $('#teste').css('background-color', '#ccc').css('padding', '20px')
+
+    setTimeout(function(){
+        $('#teste.artigo1').css('background-color', 'green')
+        $('.artigo1 > p').css('color', 'red')
+    }, 2000)
+})

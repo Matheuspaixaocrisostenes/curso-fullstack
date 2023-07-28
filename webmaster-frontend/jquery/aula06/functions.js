9091bf8290df513e6bdd9fd5cd9a4698988c5ca8
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2023-07-28 05:51:46 -0300

    aprendendo a manipular textos dinamicamente

---
+++ webmaster-frontend/jquery/aula06/functions.js
@@ -0,0 +1,15 @@
+$(function(){
+    // var elWidth =$('.box').width()
+
+    // console.log(elWidth);
+
+    $('.box').width(900)
+
+    console.log($('.box').width());
+    console.log($('.box').innerWidth());
+    console.log($('.box').outerWidth(true));
+
+    console.log($('.box').height());
+    console.log($('.box').innerHeight());
+    console.log($('.box').outerHeight(true));
+})

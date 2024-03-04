1e0ad3601f485abe493e3f84bfe8fcb4892b5c6a
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2024-03-04 05:56:56 -0300

    criando o form de login

--- javascript-completo/projetos/instagram-clone/src/App.js
+++ javascript-completo/projetos/instagram-clone/src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
 import { db } from './firebase.js'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import logo from './logo.png'
 
 function App() {
+
+  const [user, setUser] = useState('Guilherme')
+
   useEffect(() => {
-    console.log(db)
+    
   }, [])
 
   return (
     <div className="App">
-      
+      <div className='header'>
+        <div className='center'>
+        <div className='header_logo'>
+          <a href='#'><img src={logo} /></a>
+          
+        </div>
+
+        {
+          (user)?
+          <div>Olá Guilherme</div>
+          :
+          <div className='header_loginForm'>
+          <form>
+            <input type='text' placeholder='login' />
+            <input type='password' placeholder='senha' />
+            <input type='submit' name='acao' value='Logar' />
+          </form>
+        </div>
+        }
+
+        
+
+
+      </div>
+      </div>
     </div>
   );
 }

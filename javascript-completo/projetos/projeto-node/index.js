fc21d491fe57fad6b809e888e08a49bdcc018fe4
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2024-04-12 05:43:53 -0300

    criando projeto

--- javascript-completo/projetos/projeto-node/index.js
+++ javascript-completo/projetos/projeto-node/index.js
@@ -8,8 +8,10 @@ app.set('view engine', 'html')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 
+var tarefas = ['Arrumar o quarto', 'Comprar no supermecado']
+
 app.get('/', (req,res) => {
-    res.render('index', {nome: 'guilherme'})
+    res.render('index', {tarefasList:tarefas})
 })
 
 app.listen(5000, ()=>{
